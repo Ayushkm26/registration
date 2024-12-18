6f42a3f61d8a9b8b27088bb8e89b82585598d258
Ayushkm26 <ayushmishra9430@gmail.com>
2024-12-19 03:19:54 +0530

    no need

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
-const express = require("express");
-const app=express();
-const users=[{
-    name :"Ayush",
-    kidneys:[{
-        healty:false
-    }]
+const express =require("express")
+const zod=require("zod")
+const cors=require("cors")
+const app=express()
+app.use(cors())
+app.use(express.json())
+const users=[];
 
-}];
-app.use(express.json());
-app.get("/",function(req,res){
-   const jhonkidney=users[0].kidneys ;
-   const numberofKidneys=jhonkidney.length;
-   let numberofhealtykidney=0;
-   for(let i=0;i<numberofKidneys;i++){
-    if(jhonkidney[i].healty){
-        numberofhealtykidney ++;
-    }
-   }
-   let numberofunhealtykidney=numberofKidneys-numberofhealtykidney;
-   res.json({
-    numberofKidneys,
-    numberofhealtykidney,
-    numberofunhealtykidney
-   })
+const schema = zod.object({
+    "fname":zod.string(),
+    "lname":zod.string(),
+    "age":zod.string(),
+    "email":zod.string().email(),
+    "phone":zod.string().min(10),
+    "bloodgroup":zod.string(),
+    "sublocality":zod.string(),
+    "district":zod.string(),
+    "state":zod.string(),
+    "country":zod.string()
+
 })
-   app.post("/",function(req,res){
-    const ishealty=req.body.ishealty;
-    users[0].kidneys.push({
-       healty:ishealty 
-    })
-    res.json({
-        msg:"done"
-    })
-   })
-   app.put("/",function(req,res){
-     for(let i=0;i<users[0].kidneys.length;i++){
-         users[0].kidneys[i].healty=true;
-     }
-   })
-   app.delete("/",function(req,res){
-      const newKidneys=[];
-          
-   })
-  
-app.listen(3001,()=>{
-    console.log("port found");
+
+app.post("/form",(req ,res)=>{
+   
+    const response =schema.safeParse(req.body)
+    
+    if(!response.success){
+      res.status(411 ).json({
+           msg :"wrong input"
+           
+      })
+      console.log(response.error)
+      return
+    }
+ users.push(response.data)
+ console.log(users)
+
+})
+
+app.listen(3000,()=>{
+    console.log("server connected");
 })
